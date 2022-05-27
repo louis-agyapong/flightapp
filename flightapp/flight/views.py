d8d6b3b823c33bca529b87459bc00abd282e41d6
Louis Agyapong <agyapong.louis@gmail.com>
2022-05-27 05:37:29 +0000

    model viesets and urls

--- flightapp/flight/views.py
+++ flightapp/flight/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Flight, Passenger, Reservation
+from .serializers import FlightSerializer, PassengerSerializer, ReservationSerializer
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class PassengerViewSet(viewsets.ModelViewSet):
+    queryset = Passenger.objects.all()
+    serializer_class = PassengerSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
